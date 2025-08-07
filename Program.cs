4672928786989af77db9f6e09e8ae8f8fcb5edf1
troy0915 <johntroydolom7@gmail.com>
2025-08-07 15:17:46 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_8_Random_Number_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> originalNumbers = new List<int>();
+            Console.WriteLine("Enter 10 numbers:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Number {i + 1}: ");
+                while (true)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int number))
+                    {
+                        originalNumbers.Add(number);
+                        break; // Exit the loop if input is valid
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                    }
+                }
+            }
+            List<int> filteredNumbers = originalNumbers.Where(n => n > 50).ToList();
+            Console.WriteLine($"\nCount of original numbers: {originalNumbers.Count}");
+            Console.WriteLine($"Count of filtered numbers (greater than 50): {filteredNumbers.Count}");
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Activity 8 Random Number Filter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Activity 8 Random Number Filter")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a4d2e184-516f-40db-8170-ad8d09f13606")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
